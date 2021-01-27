ad595f6bf7060c7772533e44115faab725f85fdc
Robert Williams <robert@increscent.org>
2021-01-26 23:56:46 -0600

    updated sync

--- sync.sh
+++ sync.sh
@@ -44,10 +44,19 @@ download_track() {
     eval "$REQUEST"
 }
 
+remove_slash() {
+    echo "$1" | tr -d /
+}
+
+echo "Getting list of tracks from collection..."
+
 build_url "${BASE_URL}/users/${USER_ID}/favorites/tracks?countryCode=${COUNTRY_CODE}&limit=1000"
 
 eval "$REQUEST" | node parse_tracks.js | while read TRACK; do
     eval parse_track $TRACK
+
+    TRACK_TITLE=$(remove_slash "$TRACK_TITLE")
+    TRACK_ARTIST=$(remove_slash "$TRACK_ARTIST")
 
     DIR="${TARGET_DIR}/${TRACK_ARTIST}"
     FILENAME="${DIR}/${TRACK_TITLE}${FILE_EXT}"
@@ -55,6 +64,13 @@ eval "$REQUEST" | node parse_tracks.js | while read TRACK; do
     mkdir -p "$DIR"
 
     if [ ! -f "$FILENAME" ]; then
+        echo
+        echo "Downloading..."
+        echo "$TRACK_TITLE"
+        echo "$TRACK_ID"
         get_stream "$TRACK_ID" | download_track "$FILENAME"
     fi
 done
+
+echo
+echo "Done!"
